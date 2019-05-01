882e2ff42172e74d08652348af049d45a0721ee8
aliarjomandbigdeli <aliabigdeli@gmail.com>
2019-05-01 23:34:08 +0430

    add FCM class

--- run.py
+++ run.py
@@ -4,6 +4,41 @@
 """
 
 
+class FCM:
+    def __init__(self):
+        self._fuzziness = 2
+        self._error = 0.01
+        self._cluster_number = None
+        self._data = None
+
+    def exec(self, cluster_number, max_iter, data):
+        self._cluster_number = cluster_number
+        self._data = data
+
+        self.initialize_membership()
+
+        for i in range(max_iter):
+            self.calculate_cluster_centers()
+
+            self.update_membership_values()
+
+            if self.check_convergence() < self._error:
+                break
+
+    def initialize_membership(self):
+        print('initialize_membership')
+
+    def calculate_cluster_centers(self):
+        print('calculate_cluster_centers')
+
+    def update_membership_values(self):
+        print('update_membership_values')
+
+    def check_convergence(self):
+        print('check_convergence')
+        return 0
+
+
 def main():
     print('hello')
 
