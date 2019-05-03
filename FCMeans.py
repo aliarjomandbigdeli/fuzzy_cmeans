2e9f05824e623977fe65500919c9a9dce33d16d4
aliarjomandbigdeli <aliabigdeli@gmail.com>
2019-05-03 11:55:35 +0430

    make FCMeans.py

---
+++ FCMeans.py
@@ -0,0 +1,116 @@
+from sklearn.datasets.samples_generator import make_blobs
+import random
+
+"""
+.. sectionauthor:: Ali ArjomandBigdeli  <https://github.com/aliarjomandbigdeli>
+.. since:: 5/1/2019
+"""
+
+
+def distance(p1, p2):
+    s = 0
+    for i in range(len(p1)):
+        s += (p1[i] - p2[i]) ** 2
+    return s ** 0.5
+
+
+class FCM:
+    """
+    This is a class for fuzzy c-means clustering.
+    This code implemented based on https://home.deib.polimi.it/matteucc/Clustering/tutorial_html/cmeans.html
+
+    Attributes:
+        _u: membership matrix in the format of list of list.
+        _data: your dataset in the format of list of list for 2D input, for other dimension you can input similar
+         format. You set it or use create_random_data method.
+        _dimension: dimension of data or number of features of data
+    """
+
+    def __init__(self):
+        self._u = []  # membership matrix
+        self._u_previous = []  # membership matrix in previous iteration
+        self._data = []
+        self._cluster_centers = []
+        self._fuzziness = 2
+        self._error = 0.01
+        self._cluster_number = 0
+        self._dimension = 2  # number of features
+
+    def u(self):
+        """:returns membership matrix"""
+        return self._u
+
+    def data(self, d=None):
+        """getter and setter of data"""
+        if d: self._data = d
+        return self._data
+
+    def cluster_centers(self):
+        """:returns cluster centers"""
+        return self._cluster_centers
+
+    def exec(self, cluster_number, max_iter, data):
+        self._cluster_number = cluster_number
+        self._data = data
+
+        self.initialize_membership()
+
+        for i in range(max_iter):
+            self.calculate_cluster_centers()
+
+            self.update_membership_values()
+
+            if self.check_convergence() < self._error:
+                break
+
+    def create_random_data(self, num_of_data, dimension, cluster_number):
+        x, y = make_blobs(n_samples=num_of_data, centers=cluster_number, n_features=dimension)
+        self._dimension = dimension
+        self._data = x
+
+    def initialize_membership(self):
+        max_range = max(self._data[:, 0])
+        min_range = min(self._data[:, 0])
+
+        for i in range(len(self._data)):
+            s = 0
+            self._u.append([])
+            self._u_previous.append([])
+            for j in range(self._cluster_number):
+                self._u[i].append(random.uniform(min_range, max_range))
+                s += self._u[i][j]
+                self._u_previous[i].append(0)
+            for j in range(self._cluster_number):
+                self._u[i][j] /= s
+
+    def calculate_cluster_centers(self):
+        self._cluster_centers.clear()
+        for i in range(self._cluster_number):
+            cluster_center_row = []
+            for j in range(self._dimension):
+                sum1 = 0
+                sum2 = 0
+                for k in range(len(self._data)):
+                    powered = self._u[k][i] ** self._fuzziness
+                    sum1 += powered * self._data[k][j]
+                    sum2 += powered
+                cluster_center_row.append(sum1 / sum2)
+            self._cluster_centers.append(cluster_center_row)
+
+    def update_membership_values(self):
+        for i in range(len(self._data)):
+            for j in range(self._cluster_number):
+                self._u_previous[i][j] = self._u[i][j]
+                s = 0
+                upper = distance(self._data[i], self._cluster_centers[j])
+                for k in range(self._cluster_number):
+                    lower = distance(self._data[i], self._cluster_centers[k])
+                    s += (upper / lower) ** (2 / (self._fuzziness - 1))
+                self._u[i][j] = 1 / s
+
+    def check_convergence(self):
+        s = 0
+        for i in range(len(self._data)):
+            for j in range(self._cluster_number):
+                s += (self._u[i][j] - self._u_previous[i][j]) ** 2
+        return s
--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-from sklearn.datasets.samples_generator import make_blobs
 import matplotlib.pyplot as plt
-import random
+import FCMeans
 
 """
 .. sectionauthor:: Ali ArjomandBigdeli  <https://github.com/aliarjomandbigdeli>
@@ -8,121 +7,30 @@ import random
 """
 
 
-class FCM:
-    def __init__(self):
-        self._u = []  # membership matrix
-        self._u_previous = []
-        self.cluster_centers = []
-        self._fuzziness = 2
-        self._error = 0.01
-        self._cluster_number = 3
-        self._dimension = 2  # number of features
-        self.data = []
-
-    def exec(self, cluster_number, max_iter, data):
-        self._cluster_number = cluster_number
-        self.data = data
-
-        self.initialize_membership()
-
-        for i in range(max_iter):
-            self.calculate_cluster_centers()
-
-            self.update_membership_values()
-
-            if self.check_convergence() < self._error:
-                break
-
-    def create_random_data(self, num_of_data, dimension, cluster_number):
-        x, y = make_blobs(n_samples=num_of_data, centers=cluster_number, n_features=dimension)
-        self._dimension = dimension
-        self.data = x
-
-    def initialize_membership(self):
-        for i in range(len(self.data)):
-            sum = 0
-            self._u.append([])
-            self._u_previous.append([])
-            for j in range(self._cluster_number):
-                self._u[i].append(random.uniform(0.0, 1.0))
-                sum += self._u[i][j]
-                self._u_previous[i].append(0)
-            for j in range(self._cluster_number):
-                self._u[i][j] /= sum
-
-        # for k in range(len(self.data)):
-        #     print('\n')
-        #     for l in range(self._cluster_number):
-        #         print(self._u[k][l])
-        print('initialize_membership')
-
-    def calculate_cluster_centers(self):
-        self.cluster_centers.clear()
-        for i in range(self._cluster_number):
-            cluster_center_row = []
-            for j in range(self._dimension):
-                sum1 = 0
-                sum2 = 0
-                for k in range(len(self.data)):
-                    powered = self._u[k][i] ** self._fuzziness
-                    sum1 += powered * self.data[k][j]
-                    sum2 += powered
-                cluster_center_row.append(sum1 / sum2)
-            self.cluster_centers.append(cluster_center_row)
-
-        print('calculate_cluster_centers')
-
-    def update_membership_values(self):
-        for i in range(len(self.data)):
-            for j in range(self._cluster_number):
-                self._u_previous[i][j] = self._u[i][j]
-                sum = 0
-                upper = self.distance(self.data[i], self.cluster_centers[j])
-                for k in range(self._cluster_number):
-                    lower = self.distance(self.data[i], self.cluster_centers[k])
-                    sum += (upper / lower) ** (2 / (self._fuzziness - 1))
-                self._u[i][j] = 1 / sum
-
-        print('update_membership_values')
-
-    def distance(self, p1, p2):
-        sum = 0
-        for i in range(len(p1)):
-            sum += (p1[i] - p2[i]) ** 2
-        return sum ** 0.5
-
-    def check_convergence(self):
-        print('check_convergence')
-        sum = 0
-        for i in range(len(self.data)):
-            for j in range(self._cluster_number):
-                sum += (self._u[i][j] - self._u_previous[i][j]) ** 2
-        return sum
-
-
 def main():
     cluster_num = input("Enter number of cluster: ")
     data_num = input("Enter number of data(data set size): ")
-    my_fcm = FCM()
-    # print(cluster_num)
-    # my_fcm.create_random_data(data_num, 2, cluster_num)
+
+    my_fcm = FCMeans.FCM()
     my_fcm.create_random_data(int(data_num), 2, int(cluster_num))
-    my_fcm.exec(int(cluster_num), 100, my_fcm.data)
+    my_fcm.exec(int(cluster_num), 100, my_fcm.data())
 
     print('data: ')
-    print(my_fcm.data)
-    for pt in my_fcm.data:
+    print(my_fcm.data())
+
+    for pt in my_fcm.data():
         plt.plot(pt[0], pt[1], 'bo')
-    # plt.plot(my_fcm.data, 'bo')
-    for ct in my_fcm.cluster_centers:
-        plt.plot(ct[0], ct[1], 'ro')
+    for ct in my_fcm.cluster_centers():
+        plt.plot(ct[0], ct[1], 'r*')
     plt.grid(b=None, which='both', axis='both', color='gray', linestyle='-', linewidth=2)
+    plt.title('number of clusters: ' + cluster_num + ', number of data: ' + data_num)
     plt.show()
 
     print('centers: ')
-    print(my_fcm.cluster_centers)
-    # my_fcm.initialize_membership()
-    print('hello')
+    print(my_fcm.cluster_centers())
+
+    print('membership: ')
+    print(my_fcm._u)
 
 
 if __name__ == '__main__':
